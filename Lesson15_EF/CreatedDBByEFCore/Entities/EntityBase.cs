1f075349c31d427213d7f1704bf4d8d9782b2911
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2025-02-13 21:31:32 +0700

    lesson 15: ly thuyet EF

---
+++ Lesson15_EF/CreatedDBByEFCore/Entities/EntityBase.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreatedDBByEFCore.Entities
+{
+    public class EntityBase
+    {
+        // EF core mặc định, key Id thì nó hiểu đây là khoá chính
+        public Guid Id { get; set; }
+        // [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        /*kiểu dữ liệu khoá chính
+         * Dự án nhỏ: Int (DatabaseGeneratedOption.Identity), Guid
+         * Dự án lớn (Phân tán) : Snowflake-alike ID
+         */
+        public DateTime CreatedDate { get; set; }
+        //public EntityStatus Status { get; set; }
+    }
+}
